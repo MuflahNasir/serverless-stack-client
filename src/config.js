f42a19865fb874e83b2a2b14515104f7b34122e7
Syed Danial Munsif <danial.munsif@quantis.it>
2020-04-29 15:27:16 +0500

    Setting up React App

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "notes-list-app-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://646o7o1w6i.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_3rOUOhxuw",
+      APP_CLIENT_ID: "1hcb7jqhnmqflr11mu8hrnfegf",
+      IDENTITY_POOL_ID: "us-east-1:a22f8253-495b-4882-a256-af76eb2caaa1"
+    }
+  };
